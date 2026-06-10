d1ed5b02d29474a4612f901585d1f064ce4f27c5
kawecz <kawecezarjesus@gmail.com>
2026-06-10 12:54:29 -0500

    new release v2.1.4 - fixing review bot errors for the code

--- src/view.ts
+++ src/view.ts
@@ -1077,11 +1077,7 @@ export class GalleryDashboardView extends ItemView {
 					.setIcon("trash")
 					.onClick(() => {
 						void (async () => {
-							if (this.app.fileManager.trashFile) {
-								await this.app.fileManager.trashFile(item);
-							} else {
-								await this.app.vault.trash(item, true);
-							}
+							await this.app.fileManager.trashFile(item);
 							await this.renderCanvas();
 						})();
 					});
